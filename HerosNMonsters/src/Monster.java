67d2d35d133900fa7a5f8d43c5b7187b6034d4d3
Molnar Mike <Mike.Molnar@efaflex.com>
2025-09-30 10:03:38 +0200

    Rambozambo

--- HerosNMonsters/src/Monster.java
+++ HerosNMonsters/src/Monster.java
@@ -1,16 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Monster {
     private Weapon monsterWeapon;
-    private MonsterPrefix monsterPrefix;
     private int monsterXpDrop;
     private int monsterGoldDrop;
     //List<Item> MonsterLoot = new ArrayList<Item>();
 
-    public Monster(Weapon monsterWeapon, MonsterPrefix monsterPrefix, int monsterXpDrop, int monsterGoldDrop){
+    public Monster(Weapon monsterWeapon, int monsterXpDrop, int monsterGoldDrop){
         this.monsterWeapon = monsterWeapon;
-        this.monsterPrefix = monsterPrefix;
         this.monsterXpDrop = monsterXpDrop;
         this.monsterGoldDrop = monsterGoldDrop;
     }
